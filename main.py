fe902576b94130aef51044a16adb6275dced7aad
James Xie <jamesxie2019@u.northwestern.edu>
2019-05-15 16:36:40 -0500

    updated main

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import serial
 
 arduino = serial.Serial(port='/dev/cu.usbmodem143401', baudrate=9600, timeout=.1)
-prevLine = ""
+prevaccelX = 0
+prevaccelY = 0
 
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
-		print(data.decode("utf-8").split(',')[1:])
+		dataAsString = data.decode("utf-8").split(',')[1:]
+		xaccel = dataAsString[0]
+		yaccel = dataAsString[1]
+		#...
+		xjerk =  (xaccel - prevaccelX)/.005
 		#calculate jerk as currentdata - prevline / 5 ms 
 		#data - prevLine 
-		prevLine = data
+		prevaccelX = xaccel
 
